745a8d7ddf009b13889e0d88d48709f39775209d
Westin Humble <westin.h@gmail.com>
2022-01-13 19:32:44 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution in this file!
+var customerName = "bob";
+var bestCustomer;
+const leastFavoriteCustomer = "tony";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+
+};
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}; 
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+};
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "boomer";
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
